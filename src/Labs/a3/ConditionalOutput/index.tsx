2918eab9880f16f4a499a34784b22bb0eb9efe8d
Brian Brown <brianbrown2002@gmail.com>
2024-02-19 12:55:27 -0500

    a3 lab finished

---
+++ src/Labs/a3/ConditionalOutput/index.tsx
@@ -0,0 +1,13 @@
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInline from "./ConditionalOutputInine";
+const ConditionalOutput = () => {
+ return(
+   <>
+     <ConditionalOutputIfElse/>
+     <ConditionalOutputInline/>
+   </>
+ );
+};
+export default ConditionalOutput;
+
+
--- src/Labs/a3/index.tsx
+++ src/Labs/a3/index.tsx
@@ -1,8 +1,25 @@
+import Classes from "./Classes";
+import ConditionalOutput from "./ConditionalOutput";
+import Highlight from "./Highlight";
 import JavaScript from "./JavaScript"
+import Styles from "./Styles";
+import Add from "./add";
+import PathParameters from "./routing/PathParameters";
+import TodoList from "./todos/TodoList";
+
 function Assignment3() {
     return (
       <div className="container">
         <h1>Assignment 3</h1>
+        <TodoList/>
+        <Add a={3} b={4} /> <br />
+        <Highlight>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam ipsa quibusdam laboriosam esse est excepturi minus ea nulla id itaque, eius mollitia sint dolore saepe totam eos dolores doloribus eum!
+        </Highlight>
+        <ConditionalOutput/>
+        <Styles/>
+        <Classes/>
+        <PathParameters/>
         <JavaScript/>
       </div>
     );
