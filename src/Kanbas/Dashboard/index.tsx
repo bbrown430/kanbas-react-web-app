d5c0ea85cfce562408ddf626c6463ba20dd5f488
Brian Brown <brianbrown2002@gmail.com>
2024-03-19 13:44:05 -0400

    3.1 done

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,12 +1,54 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { courses } from "../Database";
+import * as db from "../Database";
 import { FaEdit } from "react-icons/fa";
 import './index.css';
 
 function Dashboard() {
+  const [courses, setCourses] = useState(db.courses);
+  const [course, setCourse] = useState({
+    _id: "0", name: "New Course", number: "New Number",
+    startDate: "2023-09-10", endDate: "2023-12-15",
+    image: "/images/reactjs.jpg"
+  });
+  const addNewCourse = () => {
+    const newCourse = { ...course,
+                        _id: new Date().getTime().toString() };
+    setCourses([...courses, { ...course, ...newCourse }]);
+  };
+  const deleteCourse = (courseId: string) => {
+    setCourses(courses.filter((course) => course._id !== courseId));
+  };
+  const updateCourse = () => {
+    setCourses(
+      courses.map((c) => {
+        if (c._id === course._id) {
+          return course;
+        } else {
+          return c;
+        }
+      })
+    );
+  };
+
   return (
     <div className="p-4">
       <h1>Dashboard</h1>
+      <h5>Course</h5>
+      <input value={course.name} className="form-control"
+             onChange={(e) => setCourse({ ...course, name: e.target.value }) } />
+      <input value={course.number} className="form-control"
+             onChange={(e) => setCourse({ ...course, number: e.target.value }) } />
+      <input value={course.startDate} className="form-control" type="date"
+             onChange={(e) => setCourse({ ...course, startDate: e.target.value }) }/>
+      <input value={course.endDate} className="form-control" type="date"
+             onChange={(e) => setCourse({ ...course, endDate: e.target.value }) } />
+      <button onClick={addNewCourse} >
+        Add
+      </button>
+      <button onClick={updateCourse} >
+        Update
+      </button>
       <hr />
       <h2>Published Courses ({courses.length})</h2>
       <hr />
@@ -23,6 +65,19 @@ function Dashboard() {
                   <Link to={`/Kanbas/Courses/${course._id}/Home`} style={{color: "grey"}}>
                         <FaEdit/>
                     </Link>
+                    <button onClick={(event) => {
+                        event.preventDefault();
+                        setCourse(course);
+                      }}>
+                      Edit
+                    </button>
+                    <button onClick={(event) => {
+                        event.preventDefault();
+                        deleteCourse(course._id);
+                      }}>
+                      Delete
+                    </button>
+
                 </div>
               </div>
             </div>
