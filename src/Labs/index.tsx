87c5a2f7f86056dee3b0514971e9df703d5700e6
Brian Brown <brianbrown2002@gmail.com>
2024-03-19 13:09:53 -0400

    finished lab 4

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,25 +1,29 @@
 import Nav from "../Nav";
 import Assignment3 from "./a3";
 import Assignment4 from "./a4";
+import store from "./store";
+import { Provider } from "react-redux";
 import {Routes, Route, Navigate}
   from "react-router";
 import { Link } from "react-router-dom";
 function Labs() {
  return (
-   <div>
-     <Nav/>
-     <Link to="/Labs/a3">Assignment 3</Link> |
-     <Link to="/Labs/a4">Assignment 4</Link>
-     <Routes>
-       <Route path="/"
-        element={<Navigate
-                  to="a3"/>}/>
-       <Route path="a3"
-        element={<Assignment3/>}/>
-       <Route path="a4"
-        element={<Assignment4/>}/>
-     </Routes>
-   </div>
+    <Provider store={store}>
+      <div>
+        <Nav/>
+        <Link to="/Labs/a3">Assignment 3</Link> |
+        <Link to="/Labs/a4">Assignment 4</Link>
+        <Routes>
+          <Route path="/"
+            element={<Navigate
+                      to="a3"/>}/>
+          <Route path="a3"
+            element={<Assignment3/>}/>
+          <Route path="a4"
+            element={<Assignment4/>}/>
+        </Routes>
+      </div>
+   </Provider>
  );
 }
 export default Labs;
